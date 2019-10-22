83e13fc37aa1e9a2d7d74279bd74ba0ee9ff69f3
Luke Rushworth <ldjrushworth@icloud.com>
2019-10-22 23:23:29 +0100

    refactors to modularise class methods

--- src/Instafetch.js
+++ src/Instafetch.js
@@ -1,62 +1,71 @@
 const axios = require("axios");
-const { client_id, client_secret, refresh_token } = process.env;
 
 class Instafetch {
   constructor() {
-    this.generateAccessToken = this.generateAccessToken.bind(this);
-    this.generateGSheetsAppendCallUrl = this.generateGSheetsAppendCallUrl.bind(
-      this
-    );
     this.appendCall = this.appendCall.bind(this);
-    this.instructions = this.instructions.bind(this);
-    this.methodNotAllowed = this.methodNotAllowed.bind(this);
+  }
+
+  fetchInstagramProfileData(username) {
+    return axios
+      .get(`https://www.instagram.com/${username}/?__a=1`)
+      .then(({ data: { graphql: { user: { is_verified } } } }) => {
+        if (!is_verified) {
+          return Promise.reject({
+            message: "Instagram profile is not verified"
+          });
+        }
+      });
   }
 
   generateAccessToken() {
+    const { client_id, client_secret, refresh_token } =
+      require("../credentials.json") || process.env;
     return axios.post(
       `https://oauth2.googleapis.com/token?client_id=${client_id}&client_secret=${client_secret}&refresh_token=${refresh_token}&grant_type=refresh_token`
     );
   }
 
-  generateGSheetsAppendCallUrl(reqSpreadsheet, reqRange) {
-    let spreadsheet = "1B05lCFpWSxkNA7kdQavjZa0VARlbx21D2TMkrGcp0Os";
-    let range = "A1";
-    if (reqSpreadsheet) spreadsheet = reqSpreadsheet;
-    if (reqRange) range = reqRange;
-    return `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheet}/values/${range}:append?includeValuesInResponse=false&insertDataOption=INSERT_ROWS&responseDateTimeRenderOption=SERIAL_NUMBER&responseValueRenderOption=FORMATTED_VALUE&valueInputOption=RAW`;
+  postToGoogleSheets(
+    spreadsheet = "1B05lCFpWSxkNA7kdQavjZa0VARlbx21D2TMkrGcp0Os",
+    range = "A1",
+    lineHash,
+    username,
+    access_token
+  ) {
+    return axios.post(
+      `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheet}/values/${range}:append?includeValuesInResponse=false&insertDataOption=INSERT_ROWS&responseDateTimeRenderOption=SERIAL_NUMBER&responseValueRenderOption=FORMATTED_VALUE&valueInputOption=RAW`,
+      {
+        range,
+        majorDimension: "ROWS",
+        values: [[lineHash, username, "true", Date.now()]]
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${access_token}`,
+          "Content-Type": "application/json"
+        }
+      }
+    );
   }
 
   appendCall({ body, query }, res) {
+    const { SpreadsheetId, SpreadsheetRange } = body;
     const { LineHash, AuthorProfile } = query;
-    const { SpreadsheetId: reqSpreadsheet, SpreadsheetRange: reqRange } = body;
-    return axios
-      .get(`https://www.instagram.com/${AuthorProfile}/?__a=1`)
-      .then(({ data }) => {
-        return data.graphql.user.is_verified;
-      })
-      .then(isVerified => {
-        if (!isVerified)
-          res.send({ message: "Instagram profile is not verified" });
+    this.fetchInstagramProfileData(AuthorProfile)
+      .then(() => {
         return this.generateAccessToken();
       })
-      .then(({ data }) => {
-        axios.post(
-          this.generateGSheetsAppendCallUrl(reqSpreadsheet, reqRange),
-          {
-            range: this.spreadsheetRange,
-            majorDimension: "ROWS",
-            values: [[LineHash, AuthorProfile, "true", Date.now()]]
-          },
-          {
-            headers: {
-              Authorization: `Bearer ${data.access_token}`,
-              "Content-Type": "application/json"
-            }
-          }
+      .then(({ data: { access_token } }) => {
+        return this.postToGoogleSheets(
+          SpreadsheetId,
+          SpreadsheetRange,
+          LineHash,
+          AuthorProfile,
+          access_token
         );
-        res.send({ message: "row added successfully" });
       })
-      .catch(err => console.log(err));
+      .then(() => res.send({ message: "row added successfully" }))
+      .catch(err => res.send(err));
   }
 
   instructions(req, res) {
