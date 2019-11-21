4a1d06aea672d7f6ba1fe5b24c89b22013f16d24
Luke Rushworth <ldjrushworth@icloud.com>
2019-11-21 21:59:55 +0000

    allows user to accept/not accept verified users, records all instagram data

--- src/Instafetch.js
+++ src/Instafetch.js
@@ -5,11 +5,19 @@ class Instafetch {
     this.appendCall = this.appendCall.bind(this);
   }
 
-  fetchInstagramProfileData(username) {
+  fetchInstagramProfileData(username, verifiedOnly) {
     return axios
       .get(`https://www.instagram.com/${username}/?__a=1`)
-      .then(({ data: { graphql: { user: { is_verified } } } }) => {
-        if (!is_verified) {
+      .then(({ data: { graphql: { user } } }) => {
+        const { is_verified } = user;
+        const userValues = Object.values(user).map(ele => {
+          if (typeof ele === "object") return JSON.stringify(ele);
+          else return ele;
+        });
+        if (verifiedOnly === "true") verifiedOnly = true;
+        if (verifiedOnly === "false") verifiedOnly = false;
+        if (!verifiedOnly || (verifiedOnly && is_verified)) return userValues;
+        if (verifiedOnly && !is_verified) {
           return Promise.reject({
             message: "Instagram profile is not verified"
           });
@@ -17,55 +25,72 @@ class Instafetch {
       });
   }
 
-  generateAccessToken() {
+  generateAccessToken(instaData) {
     const { client_id, client_secret, refresh_token } =
       require("../credentials.json") || process.env;
-    return axios.post(
-      `https://oauth2.googleapis.com/token?client_id=${client_id}&client_secret=${client_secret}&refresh_token=${refresh_token}&grant_type=refresh_token`
-    );
+    return Promise.all([
+      axios.post(
+        `https://oauth2.googleapis.com/token?client_id=${client_id}&client_secret=${client_secret}&refresh_token=${refresh_token}&grant_type=refresh_token`
+      ),
+      instaData
+    ]);
   }
 
   postToGoogleSheets(
     spreadsheet = "1B05lCFpWSxkNA7kdQavjZa0VARlbx21D2TMkrGcp0Os",
     range = "A1",
     lineHash,
-    username,
+    instaData,
     access_token
   ) {
-    return axios.post(
-      `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheet}/values/${range}:append?includeValuesInResponse=false&insertDataOption=INSERT_ROWS&responseDateTimeRenderOption=SERIAL_NUMBER&responseValueRenderOption=FORMATTED_VALUE&valueInputOption=RAW`,
-      {
-        range,
-        majorDimension: "ROWS",
-        values: [[lineHash, username, "true", Date.now()]]
-      },
-      {
-        headers: {
-          Authorization: `Bearer ${access_token}`,
-          "Content-Type": "application/json"
+    try {
+      return axios.post(
+        `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheet}/values/${range}:append?includeValuesInResponse=false&insertDataOption=INSERT_ROWS&responseDateTimeRenderOption=SERIAL_NUMBER&responseValueRenderOption=FORMATTED_VALUE&valueInputOption=RAW`,
+        {
+          range,
+          majorDimension: "ROWS",
+          values: [[new Date(), lineHash, ...instaData]]
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${access_token}`,
+            "Content-Type": "application/json"
+          }
         }
-      }
-    );
+      );
+    } catch (error) {
+      console.log(error);
+    }
   }
 
-  appendCall({ body, query }, res) {
-    const { SpreadsheetId, SpreadsheetRange } = body;
-    const { LineHash, AuthorProfile } = query;
-    this.fetchInstagramProfileData(AuthorProfile)
-      .then(() => {
-        return this.generateAccessToken();
+  appendCall(
+    {
+      query: {
+        AuthorProfile,
+        LineHash,
+        SpreadsheetId,
+        SpreadsheetRange,
+        VerifiedOnly
+      }
+    },
+    res
+  ) {
+    this.fetchInstagramProfileData(AuthorProfile, VerifiedOnly)
+      .then(instaData => {
+        return this.generateAccessToken(instaData)
+          .then(([{ data: { access_token } }, instaData]) => {
+            return this.postToGoogleSheets(
+              SpreadsheetId,
+              SpreadsheetRange,
+              LineHash,
+              instaData,
+              access_token
+            );
+          })
+          .then(() => res.send({ message: "row added successfully" }))
+          .catch(error => res.send({ innerError: error }));
       })
-      .then(({ data: { access_token } }) => {
-        return this.postToGoogleSheets(
-          SpreadsheetId,
-          SpreadsheetRange,
-          LineHash,
-          AuthorProfile,
-          access_token
-        );
-      })
-      .then(() => res.send({ message: "row added successfully" }))
-      .catch(err => res.send(err));
+      .catch(error => res.send({ outerError: error }));
   }
 
   instructions(req, res) {
